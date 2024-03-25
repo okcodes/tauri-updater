03068af30e567535e6f0d4e7693e0e3377035d7f
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-25 16:54:24 -0600

    Copy implementation from build

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,36 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { VERSION } from './version'
+import { runAssembleUpdaterCommand } from './commands/assemble-updater-command'
+import { runBuildAppCommand } from './commands/build-app-command'
+
+export type MainActionInputs = 'command'
+type Command = 'build-app' | 'assemble-updater'
+export const BUILD_APP_COMMAND = 'build-app'
+export const ASSEMBLE_UPDATER_COMMAND = 'assemble-updater'
+
+const input = (name: MainActionInputs, options: core.InputOptions): string => core.getInput(name, options)
+const VALID_COMMANDS: Command[] = [BUILD_APP_COMMAND, ASSEMBLE_UPDATER_COMMAND]
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+  console.log(`Running tauri-release-action v${VERSION}`)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  // Get command
+  const command = input('command', { required: true, trimWhitespace: true }) as Command
+  if (!VALID_COMMANDS.some(_ => _ === command)) {
+    core.setFailed(`The input "command" must be set to either "${BUILD_APP_COMMAND}" or "${ASSEMBLE_UPDATER_COMMAND}"`)
+    return
+  }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+  switch (command) {
+    case BUILD_APP_COMMAND:
+      await runBuildAppCommand()
+      break
+    case ASSEMBLE_UPDATER_COMMAND:
+      await runAssembleUpdaterCommand()
+      break
   }
 }
